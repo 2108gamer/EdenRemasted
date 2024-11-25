a1c29529a9d2f18c3607ad886578d716b418bec4
Lighdx <lapared.com@gmail.com>
2024-11-25 15:26:09 -0400

    remove un used imports

--- src/main/java/rip/diamond/practice/match/team/Team.java
+++ src/main/java/rip/diamond/practice/match/team/Team.java
@@ -10,7 +10,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
-import org.github.paperspigot.Title;
 import rip.diamond.practice.util.CC;
 import rip.diamond.practice.util.Common;
 import rip.diamond.practice.util.TitleSender;
