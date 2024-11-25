ee9fec425ea52c0e55b68ef49347e5f5656496e6
Lighdx <lapared.com@gmail.com>
2024-11-25 15:24:18 -0400

    lamba

--- src/main/java/rip/diamond/practice/match/menu/SpectateTeleportMenu.java
+++ src/main/java/rip/diamond/practice/match/menu/SpectateTeleportMenu.java
@@ -33,47 +33,45 @@ public class SpectateTeleportMenu extends PaginatedMenu {
     public Map<Integer, Button> getAllPagesButtons(Player player) {
         final Map<Integer, Button> buttons = new HashMap<>();
 
-        match.getTeamPlayers().stream().filter(TeamPlayer::isAlive).forEach(teamPlayer -> {
-            buttons.put(buttons.size(), new Button() {
-                @Override
-                public ItemStack getButtonItem(Player player) {
-                    return new ItemBuilder(Material.SKULL_ITEM)
-                            .durability(3)
-                            .headTexture(HeadUtil.getValue(teamPlayer.getPlayer()))
-                            .name(Language.MATCH_SPECTATE_TELEPORT_MENU_BUTTON_NAME.toString(teamPlayer.getUsername()))
-                            .lore(Language.MATCH_SPECTATE_TELEPORT_MENU_BUTTON_LORE.toStringList(player))
-                            .build();
-                }
+        match.getTeamPlayers().stream().filter(TeamPlayer::isAlive).forEach(teamPlayer -> buttons.put(buttons.size(), new Button() {
+            @Override
+            public ItemStack getButtonItem(Player player) {
+                return new ItemBuilder(Material.SKULL_ITEM)
+                        .durability(3)
+                        .headTexture(HeadUtil.getValue(teamPlayer.getPlayer()))
+                        .name(Language.MATCH_SPECTATE_TELEPORT_MENU_BUTTON_NAME.toString(teamPlayer.getUsername()))
+                        .lore(Language.MATCH_SPECTATE_TELEPORT_MENU_BUTTON_LORE.toStringList(player))
+                        .build();
+            }
 
-                @Override
-                public void clicked(Player player, ClickType clickType) {
-                    PlayerProfile profile = PlayerProfile.get(player);
-                    if ((profile.getPlayerState() != PlayerState.IN_MATCH && profile.getPlayerState() != PlayerState.IN_SPECTATING) || profile.getMatch() == null) {
-                        Language.MATCH_SPECTATE_TELEPORT_MENU_WRONG_STATE.sendMessage(player);
-                        return;
-                    }
-                    if (!teamPlayer.isAlive()) {
-                        Language.MATCH_SPECTATE_TELEPORT_MENU_ALREADY_DIED.sendMessage(player);
-                        return;
-                    }
-                    Player tPlayer = teamPlayer.getPlayer();
-                    if (tPlayer == null) {
-                        Language.MATCH_SPECTATE_TELEPORT_MENU_NOT_ONLINE.sendMessage(player);
-                        return;
-                    }
-                    PlayerProfile tProfile = PlayerProfile.get(tPlayer);
-                    if (tProfile.getPlayerState() != PlayerState.IN_MATCH || tProfile.getMatch() == null) {
-                        Language.MATCH_SPECTATE_TELEPORT_MENU_TARGET_WRONG_STATE.sendMessage(player);
-                        return;
-                    }
-                    if (tProfile.getMatch() != profile.getMatch()) {
-                        Language.MATCH_SPECTATE_TELEPORT_MENU_NOT_SAME_MATCH.sendMessage(player);
-                        return;
-                    }
-                    Util.teleport(player, teamPlayer.getPlayer().getLocation());
+            @Override
+            public void clicked(Player player, ClickType clickType) {
+                PlayerProfile profile = PlayerProfile.get(player);
+                if ((profile.getPlayerState() != PlayerState.IN_MATCH && profile.getPlayerState() != PlayerState.IN_SPECTATING) || profile.getMatch() == null) {
+                    Language.MATCH_SPECTATE_TELEPORT_MENU_WRONG_STATE.sendMessage(player);
+                    return;
                 }
-            });
-        });
+                if (!teamPlayer.isAlive()) {
+                    Language.MATCH_SPECTATE_TELEPORT_MENU_ALREADY_DIED.sendMessage(player);
+                    return;
+                }
+                Player tPlayer = teamPlayer.getPlayer();
+                if (tPlayer == null) {
+                    Language.MATCH_SPECTATE_TELEPORT_MENU_NOT_ONLINE.sendMessage(player);
+                    return;
+                }
+                PlayerProfile tProfile = PlayerProfile.get(tPlayer);
+                if (tProfile.getPlayerState() != PlayerState.IN_MATCH || tProfile.getMatch() == null) {
+                    Language.MATCH_SPECTATE_TELEPORT_MENU_TARGET_WRONG_STATE.sendMessage(player);
+                    return;
+                }
+                if (tProfile.getMatch() != profile.getMatch()) {
+                    Language.MATCH_SPECTATE_TELEPORT_MENU_NOT_SAME_MATCH.sendMessage(player);
+                    return;
+                }
+                Util.teleport(player, teamPlayer.getPlayer().getLocation());
+            }
+        }));
 
         return buttons;
     }
