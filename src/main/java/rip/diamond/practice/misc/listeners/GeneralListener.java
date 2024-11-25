c589a7b47633200506777fd8561e78c19f0c7609
Lighdx <lapared.com@gmail.com>
2024-11-25 16:14:29 -0400

    remove un used imports

--- src/main/java/rip/diamond/practice/misc/listeners/GeneralListener.java
+++ src/main/java/rip/diamond/practice/misc/listeners/GeneralListener.java
@@ -1,39 +1,26 @@
 package rip.diamond.practice.misc.listeners;
 
-import com.google.common.collect.ImmutableList;
 import lombok.RequiredArgsConstructor;
-import org.bukkit.Bukkit;
 import org.bukkit.Difficulty;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.*;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.hanging.HangingBreakEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
-import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.event.world.PortalCreateEvent;
 import org.bukkit.event.world.WorldLoadEvent;
 import rip.diamond.practice.Eden;
 import rip.diamond.practice.config.Config;
-import rip.diamond.practice.config.Language;
-import rip.diamond.practice.events.EdenEvent;
 import rip.diamond.practice.misc.task.EloResetTask;
-import rip.diamond.practice.party.Party;
-import rip.diamond.practice.profile.PlayerProfile;
 import rip.diamond.practice.util.CC;
-import rip.diamond.practice.util.Common;
 import rip.diamond.practice.util.TaskTicker;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 public class GeneralListener implements Listener {
@@ -112,7 +99,7 @@ public class GeneralListener implements Listener {
         }
     }
 
-    //防止踐踏農作物
+    //Evitar el pisoteo de los cultivos
     @EventHandler
     public void onCropsTrampling(PlayerInteractEvent event) {
         if (event.getAction() == Action.PHYSICAL && event.getClickedBlock().getType() == Material.SOIL) {
